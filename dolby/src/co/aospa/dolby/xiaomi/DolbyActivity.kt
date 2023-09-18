928caaa398900e7822dcbdd71ff2d8184612d6cd
Peter Kalauskas <peskal@google.com>
2023-09-18 22:10:57 +0000

    dolby: Enable use_resource_processor for all sysui deps
    This will make the build faster
    
    Test: m checkbuild
    Bug: 295208392
    Change-Id: I0c1bd901429bbe3bf81c1530e156735f8637a96e

--- dolby/src/co/aospa/dolby/xiaomi/DolbyActivity.kt
+++ dolby/src/co/aospa/dolby/xiaomi/DolbyActivity.kt
@@ -8,7 +8,6 @@ package co.aospa.dolby.xiaomi
 
 import android.os.Bundle
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
-import com.android.settingslib.widget.R
 
 private const val TAG = "DolbyActivity"
 
@@ -17,7 +16,7 @@ class DolbyActivity : CollapsingToolbarBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fragmentManager.beginTransaction()
-                .replace(R.id.content_frame, DolbySettingsFragment(), TAG)
+                .replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame, DolbySettingsFragment(), TAG)
                 .commit()
     }
 }
