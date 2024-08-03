f0e9d6badc5a0fcd9cdda0d5759d3596a0dfec00
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-03 08:36:46 +0530

    dolby: Introduce graphical equalizer
    Change-Id: I38ee6ce594e5671af42afc3d4bf0f004329482b9

---
+++ dolby/src/co/aospa/dolby/xiaomi/geq/ui/ConfirmationDialog.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2024 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.dolby.xiaomi.geq.ui
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun ConfirmationDialog(
+    text: String,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    var showDialog by remember { mutableStateOf(true) }
+    if (!showDialog) {
+        onDismiss()
+        return
+    }
+
+    AlertDialog(
+        onDismissRequest = { showDialog = false },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    showDialog = false
+                    onConfirm()
+                }
+            ) {
+                Text("Yes")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { showDialog = false }
+            ) {
+                Text("No")
+            }
+        },
+        text = {
+            Text(text)
+        }
+    )
+}
