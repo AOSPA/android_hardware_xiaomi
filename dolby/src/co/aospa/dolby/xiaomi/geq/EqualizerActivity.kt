af9edf5ac79a5a6374ec94af6154112a5d8a1685
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-03 08:36:46 +0530

    dolby: Introduce graphical equalizer
    Change-Id: I38ee6ce594e5671af42afc3d4bf0f004329482b9

---
+++ dolby/src/co/aospa/dolby/xiaomi/geq/EqualizerActivity.kt
@@ -0,0 +1,35 @@
+package co.aospa.dolby.xiaomi.geq
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import co.aospa.dolby.xiaomi.R
+import co.aospa.dolby.xiaomi.geq.ui.EqualizerScreen
+import co.aospa.dolby.xiaomi.geq.ui.EqualizerViewModel
+import com.android.settingslib.spa.framework.theme.SettingsTheme
+import com.android.settingslib.spa.widget.scaffold.SettingsScaffold
+
+class EqualizerActivity : ComponentActivity() {
+
+    private val viewModel: EqualizerViewModel by viewModels { EqualizerViewModel.Factory }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SettingsTheme {
+                SettingsScaffold(
+                    title = stringResource(id = R.string.dolby_preset)
+                ) { paddingValues ->
+                    EqualizerScreen(
+                        viewModel = viewModel,
+                        modifier = Modifier.padding(paddingValues)
+                    )
+                }
+            }
+        }
+    }
+}
