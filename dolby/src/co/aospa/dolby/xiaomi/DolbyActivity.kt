e7a5f661a927358c6812dcbbcbcad2bf47299744
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-09-25 13:05:53 +0200

    dolby: Replace widget.R with collapsingtoolbar.R
    Change-Id: I725a3bdb06f2a697791b882c5709f59b6089ba15

--- dolby/src/co/aospa/dolby/xiaomi/DolbyActivity.kt
+++ dolby/src/co/aospa/dolby/xiaomi/DolbyActivity.kt
@@ -9,7 +9,7 @@ package co.aospa.dolby.xiaomi
 import android.os.Bundle
 import co.aospa.dolby.xiaomi.preference.DolbySettingsFragment
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
-import com.android.settingslib.widget.R
+import com.android.settingslib.collapsingtoolbar.R
 
 private const val TAG = "DolbyActivity"
 
