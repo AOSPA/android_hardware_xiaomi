236ec9c5f5046758bc722a12425f4bd7d2cf2a46
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-05-17 15:52:24 +0530

    dolby: Enable bass enhancer on loudspeaker too
    It is a subtle but noticeable effect.
    
    Change-Id: I385c8474f2423c7496423716448f7480cd6acbf3

--- dolby/src/co/aospa/dolby/xiaomi/preference/DolbySettingsFragment.kt
+++ dolby/src/co/aospa/dolby/xiaomi/preference/DolbySettingsFragment.kt
@@ -231,7 +231,7 @@ class DolbySettingsFragment : PreferenceFragment(),
         resetPref.setEnabled(enable)
         hpVirtPref.setEnabled(enable && !isOnSpeaker)
         stereoPref.setEnabled(enable && !isOnSpeaker)
-        bassPref.setEnabled(enable && !isOnSpeaker)
+        bassPref.setEnabled(enable)
 
         if (!enable) return
 
@@ -259,11 +259,11 @@ class DolbySettingsFragment : PreferenceFragment(),
 
         spkVirtPref.setChecked(dolbyController.getSpeakerVirtEnabled(currentProfile))
         volumePref.setChecked(dolbyController.getVolumeLevelerEnabled(currentProfile))
+        bassPref.setChecked(dolbyController.getBassEnhancerEnabled(currentProfile))
 
         // below prefs are not enabled on loudspeaker
         if (isOnSpeaker) {
             stereoPref.summary = headphoneRes
-            bassPref.summary = headphoneRes
             hpVirtPref.summary = headphoneRes
             return
         }
@@ -276,11 +276,6 @@ class DolbySettingsFragment : PreferenceFragment(),
             } else {
                 summary = unknownRes
             }
-        }
-
-        bassPref.apply {
-            setChecked(dolbyController.getBassEnhancerEnabled(currentProfile))
-            summary = null
         }
 
         hpVirtPref.apply {
