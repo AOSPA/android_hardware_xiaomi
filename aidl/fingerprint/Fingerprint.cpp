896ac9ca887525daf52d55c6e1fe6849a5d8225b
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-07-24 13:14:32 +0200

    aidl: fingerprint: Add newly added ultra sonic modules
     * as seen on Xiaomi 15 series
    
    Change-Id: I85c51235482e58919216305aabb4554010239e5d

--- aidl/fingerprint/Fingerprint.cpp
+++ aidl/fingerprint/Fingerprint.cpp
@@ -30,8 +30,9 @@ typedef struct fingerprint_hal {
 } fingerprint_hal_t;
 
 static const fingerprint_hal_t kModules[] = {
-        {"fortsense"},  {"fpc"},         {"fpc_fod"}, {"goodix"}, {"goodix:gf_fingerprint"},
-        {"goodix_fod"}, {"goodix_fod6"}, {"silead"},  {"syna"},
+        {"fortsense"},  {"fpc"},         {"fpc_fod"},   {"goodix"},  {"goodix:gf_fingerprint"},
+        {"goodix_fod"}, {"goodix_fod6"}, {"goodix_us"}, {"qcom_us"}, {"silead"},
+        {"syna"},
 };
 
 }  // namespace
