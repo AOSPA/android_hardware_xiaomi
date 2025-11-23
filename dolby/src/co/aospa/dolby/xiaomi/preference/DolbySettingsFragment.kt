0351aa8aa0da8186c962e2de221e6dd4d760d044
Bruno Martins <bgcngm@gmail.com>
2025-11-23 21:14:35 +0000

    dolby: Convert for M3E support
    Change-Id: Ib4d1088f5be3fc2b5c2ff90d5cd1b86603551037

--- dolby/src/co/aospa/dolby/xiaomi/preference/DolbySettingsFragment.kt
+++ dolby/src/co/aospa/dolby/xiaomi/preference/DolbySettingsFragment.kt
@@ -17,7 +17,6 @@ import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.Preference.OnPreferenceChangeListener
 import androidx.preference.PreferenceCategory
-import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
 import co.aospa.dolby.xiaomi.DolbyConstants
 import co.aospa.dolby.xiaomi.DolbyConstants.Companion.PREF_BASS
@@ -35,8 +34,9 @@ import co.aospa.dolby.xiaomi.DolbyConstants.Companion.dlog
 import co.aospa.dolby.xiaomi.DolbyController
 import co.aospa.dolby.xiaomi.R
 import com.android.settingslib.widget.MainSwitchPreference
+import com.android.settingslib.widget.SettingsBasePreferenceFragment
 
-class DolbySettingsFragment : PreferenceFragmentCompat(),
+class DolbySettingsFragment : SettingsBasePreferenceFragment(),
     OnPreferenceChangeListener {
 
     private val switchBar by lazy {
